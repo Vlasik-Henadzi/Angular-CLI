899e786901d4689f5786e561f7d3ad9c8ac674ca
VlasikGG <VTB4035650@vtb.ru>
2021-10-15 13:01:31 +0300

    Single Page Applications Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Dish} from "../shared/dish";
+import { DishService } from '../service/dish.service';
 
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,10 +15,16 @@ export class DishdetailComponent implements OnInit {
   @Input()
   dish: Dish | undefined;
 
-  constructor() {
+  constructor(private dishservice: DishService,
+              private route: ActivatedRoute,
+              private location: Location) { }
+
+  ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
 
-  ngOnInit(): void {
+  goBack(): void {
+    this.location.back();
   }
-
 }
--- src/app/service/dish.service.ts
+++ src/app/service/dish.service.ts
@@ -15,9 +15,9 @@ export class DishService {
   }
 
   getDish(id: string): Dish {
-    return DISHES.filter((dish) => {
+    return DISHES.filter((dish) =>
       dish.id === id
-    })[0];
+    )[0];
   }
 
   getFeaturedDish(): Dish {
