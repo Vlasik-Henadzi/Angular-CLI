bf166650d482f68ebafc85e9ffcb8581d8b8bc6c
VlasikGG <VTB4035650@vtb.ru>
2021-10-18 19:18:41 +0300

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import {PromotionService} from "./service/promotion.service";
 import {LeaderService} from "./service/leader.service";
 
 import {AppRoutingModule} from "./app-routing/app-routing.module";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -45,9 +48,13 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatDialogModule
   ],
   providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
