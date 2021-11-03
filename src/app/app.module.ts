83d33f9b1c86d5d17f956e0f373b37fa0dae0668
VlasikGG <VTB4035650@vtb.ru>
2021-11-03 14:13:36 +0300

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import {LeaderService} from "./service/leader.service";
 
 import {AppRoutingModule} from "./app-routing/app-routing.module";
 import { LoginComponent } from './login/login.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
